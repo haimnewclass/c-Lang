e52338064669d535bcccc0a2af482c56c010af01
haim hod gilboa <haim@newclass.co.il>
2022-12-29 18:37:39 +0200

    Add project files.

---
+++ c-Lang/c-Lang.c
@@ -0,0 +1,20 @@
+
+#include <stdio.h>
+#pragma warning(disable:4996)
+
+int main()
+{
+	int a;
+	int b;
+	int c;
+
+	a = 12;
+	b = 13;
+	c = a + b;
+
+	printf("a is %d", a);
+	printf("b is %d", b);
+	printf("The result is %d", c);
+
+	return 0;
+}
