f5eff7a5f780a545c52259a6843155d95af8b758
haimnewclass <haim@newclass.co.il>
2023-01-05 20:14:17 +0200

    array

--- c-Lang/Lesson5_2.c
+++ c-Lang/Lesson5_2.c
@@ -9,19 +9,33 @@ int main()
 	int b[10];
 	// Array
 
-	b[0] = 12;
-	b[1] = 30;
-	b[2] = 40;
+	b[0] = 1;
+	b[1] = 2;
+	b[2] = 0;
+	b[3] = 3;
+	b[4] = 0;
+	b[5] = 1;
+	b[6] = 1;
+	b[7] = 100;
+	b[8] = 8;
+	b[9] = 1;
+	int counter = 0;
+	while (counter < 10)
+	{
+		printf("In house number %d has %d cars", counter,&b[counter]);
+		counter = counter + 1;
+	}
+
 	int c = 8;
 	b[c] = 90;
 	//b[10] = 89; Error
 	scanf("%d", &b[3]);
 
-	c = 0;
-	while (c<10)
+	 counter = 0;
+	while (counter <10)
 	{
-		printf("%d\n", b[c]);
-		c=c+1;
+		scanf("%d", &b[counter]);
+		counter = counter +1;
 	}
 
 
